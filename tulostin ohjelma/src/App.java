22746de9719cba9cf4f05200b0e937672ba5df1a
Aapo Eskola <aapo.eskola@elisanet.fi>
2024-10-20 17:03:26 +0300

    Päätin tehdä helpomman tehtävän kun aikaa on vähän

---
+++ tulostin ohjelma/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+    int luku1 = 5.0;
+    int luku2 = 5.0;
+    System.out.println("Hei olen Tulostin-ohjelma");
+    System.out.println("Ohjelman tekijä: Aapo");
+    
+    
+    }
+}
