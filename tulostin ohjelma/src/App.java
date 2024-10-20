816371be3b953046c7a88d5ab02add7e79d9848c
Aapo Eskola <aapo.eskola@elisanet.fi>
2024-10-20 17:09:40 +0300

    poistin skannerin ja tein tehtävän loppuun

--- tulostin ohjelma/src/App.java
+++ tulostin ohjelma/src/App.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
     
-    int luku1 = 5.0;
-    int luku2 = 5.0;
+    float luku1 = 5;
+    float luku2 = 2;
+    float tulos = (luku1 * luku2);
     System.out.println("Hei olen Tulostin-ohjelma");
     System.out.println("Ohjelman tekijä: Aapo");
-    
-    
+    System.out.println("Luku1-muuttujan arvo on " + (luku1));
+    System.out.println("Luku2-muuttujan arvo on " + (luku2));
+    System.out.println(luku1 + " * " + luku2 + " = " + tulos );
     }
 }
